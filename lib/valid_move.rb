a2e308a3b15b19a679fbfe51b019edf641b3ea6e
Nichole Battleday <battleday@gmail.com>
2019-05-07 02:30:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,24 +1,23 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 def valid_move?(board, index)
-   if board[index] == 0
-     return true
-   else
-     return false
-   end
-   if board[index] == 100
-     return false
-   else
-     return true
-   end
- end
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
 
-board = [" ", " ", "X", " ", " ", " ", " ", "O", " "]
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
 
-def valid_move?(board, index)
-   if board[index] == 2
-     return true
-   else
-     return false
-   end
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
 end
